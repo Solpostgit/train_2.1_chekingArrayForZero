7c4929a6cdc04e39b351e37e5c7874a642d111bd
Solpostgit <begemot_off@mail.ru>
2023-12-20 22:34:20 +0300

    add text

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        public
+        int [] arr = new int[] {5};
+        arr[0] = 3;
+        arr[1] = 4;
+        arr[2] = 5;
+        arr[3] = 6;
+        arr[4] = 7;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                System.out.println(arr[i]);
+            }
+        }
     }
 }
